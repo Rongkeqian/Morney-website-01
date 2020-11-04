1788f5f6b2029c41405a9024d4d427c224a6ab25
RongKeQian <qianer_lover@126.com>
2020-11-04 10:03:21 +0800

    deploy

---
+++ precache-manifest.4b66f9846c25d3075a2701e8f9bb2d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5f35033cc24e1a7a6e",
+    "url": "/Morney-website-01/css/app.261f1bcd.css"
+  },
+  {
+    "revision": "feb2401ac8da01d594820f61b0a33512",
+    "url": "/Morney-website-01/index.html"
+  },
+  {
+    "revision": "bd5f35033cc24e1a7a6e",
+    "url": "/Morney-website-01/js/app.6fdff5d0.js"
+  },
+  {
+    "revision": "0cdd934cb8f5b1427dfb",
+    "url": "/Morney-website-01/js/chunk-vendors.f6da10d5.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/Morney-website-01/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/Morney-website-01/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website-01/robots.txt"
+  }
+]);
